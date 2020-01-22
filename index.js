f2004df40300a381cca920d7aea7901453fb1691
niskarsh <niskarshk@gmail.com>
2020-01-22 16:30:52 +0530

    Sms test route added

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const bodyparser = require("body-parser");
-
-const client = require("twilio")(
+const environment = process.env.NODE_ENV;
+const clientDev = require("twilio")(
 	process.env.DEV_ACCOUNTSID,
 	process.env.DEV_AUTHTOKEN
+);
+const clientProd = require("twilio")(
+	process.env.PROD_ACCOUNTSID,
+	process.env.PROD_AUTHTOKEN
 );
 
 const app = express();
@@ -13,15 +17,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const twilioMiddleware = (req, res, next) => {
-	res.header("Access-Control-Allow-Origin", req.headers.origin);
-	res.header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
-	res.header("Access-Control-Allow-Headers", "*");
-	res.header("Access-Control-Allow-Credentials", "true");
 	res.header("Content-Type", "text/richtext");
 	next();
 };
 
 const authMiddleware = (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", req.headers.origin);
+	res.header("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+	res.header("Access-Control-Allow-Headers", "*");
+	res.header("Access-Control-Allow-Credentials", "true");
 	next();
 };
 
@@ -39,12 +43,99 @@ app.get("/", (req, res) => {
 	res.send("backend started");
 });
 
+app.post("/twilio/prod/testsms", (req, res) => {
+	clientProd.messages
+		.create({
+			from: "+16314064104",
+			body: `${req.body.Body}`,
+			to: "+916386717156"
+		})
+		.then(message => {
+			console.log(message);
+			res.send(message.sid)
+		})
+		.catch(err => {
+			console.log(err);
+			// next();
+			res.status(500).send(err);
+		});
+});
+
+app.post("/twilio/dev/testsms", (req, res) => {
+	clientDev.messages
+		.create({
+			from: "+15005550006",
+			body: `${req.body.Body}`,
+			to: "+916386717156"
+		})
+		.then(message => {
+			console.log(message);
+			res.send(message.sid);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).send(err);
+		});
+});
+
+app.get("/twilio/prod/testsms", (req, res, next) => {
+	clientProd.messages
+		.create({
+			from: "+16314064104",
+			body: `testing`,
+			to: "+916386717156"
+		})
+		.then(message => {
+			console.log(message);
+			next();
+		})
+		.catch(err => {
+			console.log(err);
+			next();
+			// res.status(500).send(err);
+		});
+});
+
+app.get("/twilio/dev/testsms", (req, res) => {
+	clientDev.messages
+		.create({
+			from: "+15005550006",
+			body: `testing`,
+			to: "+916386717156"
+		})
+		.then(message => {
+			console.log(message);
+			res.send(message);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).send(err);
+		});
+});
+
+app.get("/twilio/prod/testwhatsapp", (req, res) => {
+	clientProd.messages
+		.create({
+			from: "whatsapp:+16314064104",
+			body: `Thank you for applying for SOAL Product Engineering. Your interview is scheduled for the following date and time.`,
+			to: "whatsapp:+919807422073"
+		})
+		.then(message => {
+			console.log(message);
+			res.send(message);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).send(err);
+		});
+});
+
 app.use("/twilio", twilioMiddleware);
 
 app.post("/twilio/endpoint", (req, res) => {
-	client.messages
+	clientProd.messages
 		.create({
-			from: "+15005550006",
+			from: "+16314064104",
 			body: `${req.body.Body}`,
 			to: "+916386717156"
 		})
